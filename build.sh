c85e552c30aaea077dda10397881d112eb186658
James Pike <jpike@chilon.net>
2017-03-25 21:50:24 +0000

    add openvpn and smell-baron to container

--- build.sh
+++ build.sh
@@ -4,15 +4,28 @@ name=sonkar
 version=0.0.1
 dest_aci=$name-$version.aci
 
+mkdir -p .resources
+
 # ensure the build gets finished no matter what
 finish() {
-  acbuild end
+  sudo acbuild end
 }
-trap finish EXIT
+trap finish EXIT TERM
+
+copy_bin_from_net() {
+  fname=$(basename $2)
+  wget -c $1 -O .resources/$fname
+  chmod a+x .resources/$fname
+  sudo acbuild copy .resources/$fname $2
+}
 
 acbuild begin
 acbuild dependency add quay.io/coreos/alpine-sh
+# acbuild dependency add quay.io/aptible/busybox
 acbuild set-name $name
+copy_bin_from_net https://github.com/ohjames/smell-baron/releases/download/v0.4.2/smell-baron.musl /bin/smell-baron
+sudo acbuild set-exec /bin/smell-baron
 
-rm $dest_aci
-acbuild write $dest_aci
+sudo acbuild run apk update
+sudo acbuild run apk add openvpn
+sudo acbuild write --overwrite $dest_aci
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 
-sudo rkt run --insecure-options=image --interactive ./*.aci --exec=sh
+dns=8.8.8.8
+
+# sudo iptables -P FORWARD ACCEPT
+sudo rkt run --insecure-options=image --dns=$dns --net=default --interactive ./*.aci --exec=sh
